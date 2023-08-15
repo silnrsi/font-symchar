a2efa57c4177b2ebcc05e0397112d2ad8fc177b2
Victor Gaultney <victor_gaultney@sil.org>
2023-08-15 10:59:30 +0100

    Add glyph images and script to create them using DrawBot [nobuild]

--- tools/makeimages.py
+++ tools/makeimages.py
@@ -13,11 +13,11 @@ from drawBot import *
 import csv
 
 # Specs
-fontname = "SymCharK"
-fontsize = 36
-height = 60
-width = 180
-incsv = "../source/glyph_data_part.csv"
+fname = "SymCharK"
+fsize = 36
+height = 48
+width = 96
+incsv = "../source/glyph_data.csv"
 outfolder = "../documentation/images/"
 
 def uconvert(u):
@@ -31,16 +31,25 @@ def make_image(left, char, righta, rightb):
     fra = "" if righta == "" else uconvert(righta)
     frb = "" if rightb == "" else uconvert(rightb)
     filename = outfolder + "img_" + char[2:6] + ".png"
-    frame = fl + ch + fra + frb
+    #frame = fl + ch + fra + frb
+
+    frame = FormattedString()
+    frame.font(fname)
+    frame.fontSize(fsize)
+    frame.align("center")
+    frame.append (fl, fill=(0, 0, 0, 0.3))
+    frame.append (ch, fill=(0, 0, 0, 1))
+    frame.append (fra, fill=(0, 0, 0, 0.3))
+    frame.append (frb, fill=(0, 0, 0, 0.3))
 
     newDrawing()
     size(width, height)
     stroke(None)
     fill(1, 1, 1, 1)
     rect(0, 0, width, height)
-    font(fontname, fontsize)
-    fill(0, 0, 0, 1)
-    textBox(frame, (0, 2, width, height), align="center")
+    #font(fname, fsize)
+    #fill(0, 0, 0, 1)
+    textBox(frame, (0, -2, width, height))
         
     endDrawing()
 
@@ -52,7 +61,8 @@ def main():
         csv_reader = csv.DictReader(csvfile)
         line_count = 0
         for row in csv_reader:
-            make_image(row["frame_l"], row["usv"], row["frame_r1"], row["frame_r2"])
+            if row["usv"] != "":
+                make_image(row["frame_l"], row["usv"], row["frame_r1"], row["frame_r2"])
             line_count += 1
         print(f'Created {line_count} images')
     
