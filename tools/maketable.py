53a81a2a8314f68e1214c170f5ba577b475ede00
Victor Gaultney <victor_gaultney@sil.org>
2025-01-17 11:20:49 +0000

    Remove uniqueID lib key; Update to SIL Global; Update copyright years [nobuild]

--- tools/maketable.py
+++ tools/maketable.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 __doc__ = '''Create markdown encoding table for SymChar font docs '''
 __url__ = 'https://github.com/silnrsi/font-symchar'
-__copyright__ = 'Copyright (c) 2023 SIL International (https://www.sil.org)'
+__copyright__ = 'Copyright (c) 2023-2025 SIL Global (https://www.sil.org)'
 __license__ = 'Released under the MIT License (https://opensource.org/licenses/MIT)'
 __author__ = 'Victor Gaultney'
 
